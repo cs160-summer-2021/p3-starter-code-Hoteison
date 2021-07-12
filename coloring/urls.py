b14647340b28a955bafe217b455ac168708e2895
Hoteison <alexlauluo@berkeley.edu>
2021-07-11 23:35:41 -0700

    add django urls

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.index, name='new_interaction'),
+    path('start_screen', views.start_screen, name='start_screen'),
+    path('animals', views.animals, name='animals'),
+    path('flowers', views.flowers, name='flowers'),
+    path('landscapes', views.landscapes, name='landscapes')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def start_screen(request):
+    return render(request, 'coloring/start_screen.html')
+
+def animals(request):
+    return render(request, 'coloring/animals.html')
+
+def flowers(request):
+    return render(request, 'coloring/flowers.html')
+
+def landscapes(request):
+    return render(request, 'coloring/landscapes.html')
